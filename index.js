61bb52d88ed9e6290bbb15b08e463a80daf89b3c
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-06-11 22:05:14 +0530

    docs updates

--- index.js
+++ index.js
@@ -1,19 +1,69 @@
+/**
+ * Express Marko Generator
+ * A CLI tool to generate an Expressjs application with MarkoJS and MaterializeCSS framework.
+ * Written by: Sandeep Vattapparambil
+ * Email: sandeepv68@gmail.com
+ * Website: www.sandeepv.in
+ */
+
+ /**
+  * Figlet - render ASCII graphics in terminal
+  */
 const figlet = require('figlet');
+
+/**
+ * Chalk - colorful terminal logs
+ */
 const chalk = require('chalk');
+
+/**
+ * Clui - spinners and loaders in terminal
+ */
 const clui = require('clui');
+
+/**
+ * Replace-in-file - replace strings in files
+ */
 const replace = require('replace-in-file');
+
+/**
+ * Clui spinner instance
+ */
 const spinner = clui.Spinner;
+
+/**
+ * Custom progress message and spinner
+ */
 const progress = new spinner('\ncloning repo...  ', ['⣾', '⣽', '⣻', '⢿', '⡿', '⣟', '⣯', '⣷']);
 
+/**
+ * Exec command from child_process
+ */
 const {
     exec
 } = require('child_process');
+
+/**
+ * Application configuration object
+ */
 const config = {
     name: 'Express Marko Generator',
     version: "1.0.0"
 };
+
+/**
+ * Log - console.log instance
+ */
 const log = console.log;
+
+/**
+ * express-marko-generator github repository url
+ */
 const repoUrl = `https://github.com/SandeepVattapparambil/express-marko.git`;
+
+/**
+ * Promise object to draw and render logo ASCII
+ */
 var drawLogo = new Promise(function (resolve, reject) {
     figlet(config.name, function (err, data) {
         if (err) {
@@ -26,7 +76,13 @@ var drawLogo = new Promise(function (resolve, reject) {
     });
 });
 
+/**
+ * Application bootstrapping function
+ */
 function generate() {
+    /**
+     * process.argv[2] - command line arguments
+     */
     if (process.argv[2]) {
         let folderName = process.argv[2];
         if (folderName.length < 4) {
@@ -37,6 +93,9 @@ function generate() {
             log(chalk.red.bold(`\nError: Project name cannot contain special chars`));
             process.exit();
         }
+        /**
+         * Check git is installed or not
+         */
         exec(`git`, function (err, data) {
             if (!data) {
                 log(chalk.red.bold(`\nError: Git is not installed`));
@@ -45,12 +104,18 @@ function generate() {
             const gitCommand = `git clone ${repoUrl} ${folderName}`;
             progress.start();
             progress.message('cloning repo...');
+            /**
+             * Clone repo
+             */
             let clone = exec(gitCommand, function (err, data) {
                 if (err) {
                     log(chalk.red.bold(err));
                     process.exit();
                 }
             });
+            /**
+             * Customize project
+             */
             clone.on('close', function (code) {
                 progress.stop();
                 log(chalk.green(`\nProject files downloaded successfully.`));
