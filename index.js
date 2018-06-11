d9978bd8a2967ed8b74899329db1e48619b79237
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-06-11 20:04:02 +0530

    updates

--- index.js
+++ index.js
@@ -9,21 +9,34 @@ const config = {
 };
 const log = console.log;
 
-if (process.argv[2]) {
-    let folderName = process.argv[2];
-    // exec(`mkdir ${folderName}`);
-} else {
+var drawLogo = new Promise(function (resolve, reject) {
     figlet(config.name, function (err, data) {
         if (err) {
             log(chalk.red('Something went wrong...'));
-            console.dir(err);
-            return;
+            reject(err);
         }
         log(chalk.yellow.bold(data));
         log(chalk.blue(`v${config.version}`));
+        resolve(generate());
+    });
+});
+
+function generate() {
+    if (process.argv[2]) {
+        let folderName = process.argv[2];
+        if(folderName.length < 4 ){
+            log(chalk.red(`Error: Use a valid project name with minimum 4 chars`));
+            process.exit()
+        }
+        if(!folderName.match(/^[^\\/?%*:|"<>\.]+$/g)){
+            log(chalk.red(`Error: Project name cannot contain special chars`));
+            process.exit()
+        }
+        // exec(`mkdir ${folderName}`);
+    } else {
         log(chalk.blue(`\n[option]         [description]`));
         log(chalk.blue(` -v | --version     get version`));
         log(chalk.blue(`\nUsage:`));
         log(chalk.blue(`\express-marko-generator <project-name>`));
-    });
+    }
 }
