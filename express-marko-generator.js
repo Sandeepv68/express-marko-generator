23683643c202ffdaba57aa61768fbd8bc362ed8a
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-06-17 17:13:47 +0530

    Promise wrapper for exec added

--- express-marko-generator.js
+++ express-marko-generator.js
@@ -39,7 +39,7 @@ const spinner = clui.Spinner;
 /**
  * Custom progress message and spinner
  */
-const progress = new spinner('\ncloning repo...  ', ['⣾', '⣽', '⣻', '⢿', '⡿', '⣟', '⣯', '⣷']);
+const progress = new spinner('\nCloning repo...  ', ['⣾', '⣽', '⣻', '⢿', '⡿', '⣟', '⣯', '⣷']);
 
 /**
  * Exec command from child_process
@@ -67,9 +67,54 @@ const config = {
 const log = console.log;
 
 /**
- * express-marko-generator github repository url
+ * express-marko github repository url
  */
 const repoUrl = `https://github.com/SandeepVattapparambil/express-marko.git`;
+
+/**
+ * @name isWindows
+ * Helper function to check the current platform for Windows/Unix-ish.
+ * @returns {Boolean} - Returns true if Windows, else false.
+ */
+const isWindows = () => {
+    if (/^win/i.test(process.platform)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+/**
+ * @name run
+ * A Promise factory to execute an arbitrary shell command.
+ * @param {String} script - The arbitrary shell command to execute.
+ * @returns {<Promise>} - The promise object containing the stdout/stderr. 
+ */
+const run = (script) => {
+    return new Promise((resolve, reject) => {
+        /**
+         * If a callback function is provided, it is called with the arguments (error, stdout, stderr). 
+         * On success, error will be null. On error, error will be an instance of Error. 
+         * The error.code property will be the exit code of the child process while error.signal 
+         * will be set to the signal that terminated the process. 
+         * Any exit code other than 0 is considered to be an error.
+         * The stdout and stderr arguments passed to the callback will contain the stdout and stderr 
+         * output of the child process. By default, Node.js will decode the output as UTF-8 and pass 
+         * strings to the callback. The encoding option can be used to specify the character encoding 
+         * used to decode the stdout and stderr output. If encoding is 'buffer', or an unrecognized 
+         * character encoding, Buffer objects will be passed to the callback instead.
+         */
+        exec(script, (error, stdout, stderr) => {
+            if (stderr) {
+                //reject promise with error
+                reject(stderr);
+            } else {
+                //resolve promise with success
+                resolve(stdout);
+            }
+        });
+    });
+}
 
 /**
  * Bootstrap the generator after rendering the logo
@@ -89,6 +134,7 @@ figlet(config.name, (err, data) => {
      * check the args[] of process for parameters and then initiate the generator or else log the help.
      */
     if (process.argv[2]) {
+
         /**
          * get the argument passed as the project name.
          */
@@ -140,72 +186,72 @@ figlet(config.name, (err, data) => {
                 progress.stop();
                 log(chalk.green(`\nProject files downloaded successfully.`));
                 if (isWindows) {
+
                     /**
                      * Cleanup project folder in WINDOWS.
                      */
-                    exec(`rmdir ${projectName}\\.git /s /q`, (err) => {
-                        if (err) {
-                            log(chalk.red.bold(`\nError removing .git folder, ${err}`));
-                        }
-                    });
-                    exec(`rmdir ${projectName}\\.github /s /q`, (err) => {
-                        if (err) {
-                            log(chalk.red.bold(`\nError removing .git folder, ${err}`));
-                        }
-                    });
-                    exec(`del ${projectName}\\CODE_OF_CONDUCT.md /s /q`, (err) => {
-                        if (err) {
-                            log(chalk.red.bold(`\nError cleaning up the project, ${err}`));
-                        }
-                    });
-                    exec(`del ${projectName}\\PULL_REQUEST_TEMPLATE.md /s /q`, (err) => {
-                        if (err) {
-                            log(chalk.red.bold(`\nError cleaning up the project, ${err}`));
-                        }
-                    });
-                    exec(`del ${projectName}\\CONTRIBUTING.md /s /q`, (err) => {
-                        if (err) {
-                            log(chalk.red.bold(`\nError cleaning up the project, ${err}`));
-                        }
-                    });
-                    exec(`del ${projectName}\\LICENSE /s /q`, (err) => {
-                        if (err) {
-                            log(chalk.red.bold(`\nError cleaning up the project, ${err}`));
-                        }
+                    //remove .git directory
+                    let removeGitFolder = `rmdir ${projectName}\\.git /s /q`;
+                    //remove .github folder
+                    let removeGithubFolder = `rmdir ${projectName}\\.github /s /q`;
+                    //remove CODE_OF_CONDUCT.md
+                    let removeCoC = `del ${projectName}\\CODE_OF_CONDUCT.md /s /q`;
+                    //remove PULL_REQUEST_TEMPLATE.md
+                    let removePullReqTemp = `del ${projectName}\\PULL_REQUEST_TEMPLATE.md /s /q`;
+                    //remove CONTRIBUTING.md
+                    let removeContrib = `del ${projectName}\\CONTRIBUTING.md /s /q`;
+                    //remove LICENSE
+                    let removeLicense = `del ${projectName}\\LICENSE /s /q`;
+
+                    /**
+                     * Promisify and execute remove commands.
+                     */
+                    run(removeGitFolder).then((command1) => {
+                        return run(removeGithubFolder);
+                    }).then((command2) => {
+                        return run(removeCoC);
+                    }).then((command3) => {
+                        return run(removePullReqTemp);
+                    }).then((command4) => {
+                        return run(removeContrib);
+                    }).then((command5) => {
+                        return run(removeLicense);
+                    }).catch((error) => {
+                        log(chalk.red.bold(`\nError cleaning up the project: , ${error}`));
                     });
                 } else {
                     /**
                      * Cleanup in unix, mac, linux
                      */
-                    exec(`rm ${projectName}/.git`, function (err) {
-                        if (err) {
-                            log(chalk.red.bold(`\nError removing .git folder, ${err}`));
-                        }
-                    });
-                    exec(`rm ${projectName}/.github`, function (err) {
-                        if (err) {
-                            log(chalk.red.bold(`\nError removing .git folder, ${err}`));
-                        }
-                    });
-                    exec(`del ${projectName}/CODE_OF_CONDUCT.md`, function (err) {
-                        if (err) {
-                            log(chalk.red.bold(`\nError cleaning up the project, ${err}`));
-                        }
-                    });
-                    exec(`del ${projectName}/PULL_REQUEST_TEMPLATE.md`, function (err) {
-                        if (err) {
-                            log(chalk.red.bold(`\nError cleaning up the project, ${err}`));
-                        }
-                    });
-                    exec(`del ${projectName}/CONTRIBUTING.md`, function (err) {
-                        if (err) {
-                            log(chalk.red.bold(`\nError cleaning up the project, ${err}`));
-                        }
-                    });
-                    exec(`del ${projectName}/LICENSE`, function (err) {
-                        if (err) {
-                            log(chalk.red.bold(`\nError cleaning up the project, ${err}`));
-                        }
+
+                    //remove .git directory
+                    let removeGitFolder = `rm ${projectName}/.git`;
+                    //remove .github folder
+                    let removeGithubFolder = `rm ${projectName}/.github`;
+                    //remove CODE_OF_CONDUCT.md
+                    let removeCoC = `del ${projectName}/CODE_OF_CONDUCT.md`;
+                    //remove PULL_REQUEST_TEMPLATE.md
+                    let removePullReqTemp = `del ${projectName}/PULL_REQUEST_TEMPLATE.md`;
+                    //remove CONTRIBUTING.md
+                    let removeContrib = `del ${projectName}/CONTRIBUTING.md`;
+                    //remove LICENSE
+                    let removeLicense = `del ${projectName}/LICENSE`;
+
+                    /**
+                     * Promisify and execute remove commands.
+                     */
+                    run(removeGitFolder).then((command1) => {
+                        return run(removeGithubFolder);
+                    }).then((command2) => {
+                        return run(removeCoC);
+                    }).then((command3) => {
+                        return run(removePullReqTemp);
+                    }).then((command4) => {
+                        return run(removeContrib);
+                    }).then((command5) => {
+                        return run(removeLicense);
+                    }).catch((error) => {
+                        log(chalk.red.bold(`\nError cleaning up the project: , ${error}`));
                     });
                 }
                 let packageFiles = {
@@ -224,7 +270,7 @@ figlet(config.name, (err, data) => {
                     to: `${projectName}`
                 };
                 async.parallel({
-                    replacePackageFiles: function (callback) {
+                    replacePackageFiles: (callback) => {
                         replace(packageFiles, (error) => {
                             if (error) {
                                 return console.error('Error occurred:', error);
@@ -232,7 +278,7 @@ figlet(config.name, (err, data) => {
                         });
                         callback(null, `Package files customized.`);
                     },
-                    replaceServerFile: function (callback) {
+                    replaceServerFile: (callback) => {
                         replace(serverFile, (error) => {
                             if (error) {
                                 return console.error('Error occurred:', error);
@@ -261,16 +307,3 @@ figlet(config.name, (err, data) => {
         log(chalk.blue(`\$ express-marko-generator <project-name>`));
     }
 });
-
-/**
- * @name isWindows
- * Helper function to check the current platform for Windows/Unix-ish.
- * @returns {Boolean} - Returns true if Windows, else false.
- */
-const isWindows = () => {
-    if (/^win/i.test(process.platform)) {
-        return true;
-    } else {
-        return false;
-    }
-}
